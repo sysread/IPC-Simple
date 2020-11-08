26d75d0102e95515a1491d42ad148908562abb1d
Jeff Ober <jober@ziprecruiter.com>
2020-11-08 18:33:59 -0500

    Add timestamp to debug output

--- lib/IPC/Simple/Util.pm
+++ lib/IPC/Simple/Util.pm
@@ -13,7 +13,8 @@ sub debug {
   if ($ENV{IPC_SIMPLE_DEBUG}) {
     my $msg = sprintf shift, @_;
     my ($pkg, $file, $line) = caller;
-    warn "<$pkg : $line> $msg\n";
+    my $ts = time;
+    warn "<$pkg:$line | $ts> $msg\n";
   }
 }
 
